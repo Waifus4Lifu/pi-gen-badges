93bef0131e80874c2aec63724991c25a9317fa18
Andrew LeCody <andrew@beyondweb.net>
2019-07-06 22:31:03 -0500

    Added config and drivers for LCD screen

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,5 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/bcm2708-rpi-0-w.dtb "${ROOTFS_DIR}/boot/"
+install -m 644 files/mzdpi.dtbo "${ROOTFS_DIR}/boot/overlays/"
